9c6d81910686c629f1c87cc97b5fed5d7f262697
roy <fengmushu@gmail.com>
2016-06-16 17:24:56 +0800

    move build-in to comm nproto include 	modified:   drivers/nproto/src/rules-inner.c 	modified:   drivers/nproto/src/rules/mail.c 	renamed:    drivers/nproto/src/rules/build-in.h -> include/src/nproto/build-in.h

--- drivers/nproto/src/rules-inner.c
+++ drivers/nproto/src/rules-inner.c
@@ -3,11 +3,14 @@
 
 int inner_rules_init(void)
 {
-	extern np_rule_t inner_http_req, inner_http_rep, inner_pop3;
+	extern np_rule_t \
+	inner_http_req, inner_http_rep, \
+	inner_pop3, inner_smtp;
 
 	np_rule_register(&inner_http_req);
 	np_rule_register(&inner_http_rep);
 	np_rule_register(&inner_pop3);
+	np_rule_register(&inner_smtp);
 
 	return 0;
 }
--- drivers/nproto/src/rules/mail.c
+++ drivers/nproto/src/rules/mail.c
@@ -10,6 +10,9 @@
 #include "../mwm.h"
 #include "../bmh.h"
 
+/*
++OK Welcome to XXXmail Mail Pop3 Server
+*/
 np_rule_t inner_pop3 = {
 	.name_rule = "pop3",
 	.name_app = "e-mail",
@@ -42,8 +45,52 @@ np_rule_t inner_pop3 = {
 			{
 				.type_match = MHTP_REGEXP,
 				.offset = 0,
-				.deep = 32,
+				.deep = 48,
 				.patt = "/^\\+ok\\b.*pop3\\b/i",
+			},
+		},
+	},
+
+	/* callback's. */
+};
+
+/*
+220 XXX.cn Anti-spam GT for Coremail System
+*/
+np_rule_t inner_smtp = {
+	.name_rule = "smtp",
+	.name_app = "e-mail",
+	.name_service = "e-mail",
+
+	.ID = NP_INNER_RULE_SMTP,
+	.priority = NP_RULE_PRI_MAX,
+
+	.rule_type = TP_RULE_BASE,
+	.refs_type = NP_REF_NONE,
+
+	.enable_http = 0,
+
+	/* layer 4 match. */
+	.enable_l4 = 1,
+	.l4 = {
+		.proto = IPPROTO_TCP,
+	},
+
+	/* layer 7 context. */
+	.enable_l7 = 1,
+	.l7 = {
+		.dir = NP_FLOW_DIR_S2C,
+		.lnm = {
+			.type = NP_LNM_NONE,
+		},
+		.ctm_num = 1, /*  */
+		.ctm_relation = NP_CTM_AND,
+		.ctm = {
+			{
+				.type_match = MHTP_REGEXP,
+				.offset = 0,
+				.deep = 48,
+				.patt = "/^220[\\x09-\\x0d -~]*.*(smtp|mail)/i",
 			},
 		},
 	},
