4a6db9266200e7a8b10c6c218f3625ef5ae5a289
helloshiki <aquariusye@163.com>
2016-06-22 17:47:36 +0800

    start up sequence

--- database/files/database.sh
+++ database/files/database.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
+mkdir -p /tmp/db
+mkdir -p /data/sqlite3
+cd /usr/share/database
+./init.sh
 
+for i in 1 2 3 4 5 6 7 8 9 ; do echo $i; done
+	mysql -uroot -pwjrc0409 -e "select 1" >/dev/null 2>&1 
+	test $? -eq 0 && break 
+	sleep 1 
+done
 dir=/usr/share/database
 cd $dir
-lua $dir/main.lua
-
+lua $dir/main.lua >>/tmp/log/lua.error 2>&1
