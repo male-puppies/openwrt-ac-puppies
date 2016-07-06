a8742c85dfa6b2cb8488d53c182c255b735176ba
Chen Minqiang <ptpt52@gmail.com>
2016-07-06 15:02:58 +0800

    add nos-zone nos-ipgrp

---
+++ ac-apps/nos-ipgrp/files/nos-ipgrp.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+[ x$1 = xstop ] && {
+	exit 0
+}
+
+[ x$1 = xstart ] || {
+	echo "usage: $0 start|stop"
+	exit 0
+}
+
+echo clean >/dev/nos_ipgrp_ctl
+
+for i in `seq 0 255`; do
+	ipset destroy ipgrp_$i >/dev/null 2>&1
+done
+
+for i in `seq 0 255`; do
+	[ x"`uci get nos-ipgrp.@ipgrp[$i] 2>/dev/null`" = xipgrp ] >/dev/null 2>&1 || break
+
+	id="`uci get nos-ipgrp.@ipgrp[$i].id`"
+	test -n "$id" || continue
+	networks="`uci get nos-ipgrp.@ipgrp[$i].network`"
+	type="`uci get nos-ipgrp.@ipgrp[$i].type`"
+
+	if [ x"$type" = "xall" ]; then
+		echo ipgrp $id=@all >/dev/nos_ipgrp_ctl
+	else
+		ipset create ipgrp_$id hash:net
+		for network in $networks; do
+			ipset add ipgrp_$id $network
+		done
+		echo ipgrp $id=ipgrp_$id >/dev/nos_ipgrp_ctl
+	fi
+done
+
+exit 0
---
+++ ac-apps/nos-zone/files/nos-zone.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+[ x$1 = xstop ] && {
+	exit 0
+}
+
+[ x$1 = xstart ] || {
+	echo "usage: $0 start|stop"
+	exit 0
+}
+
+echo clean >/dev/nos_zone_ctl
+
+for i in `seq 0 255`; do
+	[ x"`uci get nos-zone.@zone[$i] 2>/dev/null`" = xzone ] >/dev/null 2>&1 || break
+
+	id="`uci get nos-zone.@zone[$i].id`"
+	test -n "$id" || continue
+	ifnames="`uci get nos-zone.@zone[$i].ifname`"
+	test -n "$ifnames" || continue
+
+	for ifname in $ifnames; do
+		echo zone $id=$ifname >/dev/nos_zone_ctl
+	done
+done
+
+exit 0
