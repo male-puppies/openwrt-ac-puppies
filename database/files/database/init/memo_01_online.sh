95a0694e3d23ef365eeda0d541637406fccfb0fc
helloshiki <aquariusye@163.com>
2016-07-11 15:28:39 +0800

    add memory table online

--- database/files/database/init/memo_01_online.sh
+++ database/files/database/init/memo_01_online.sh
@@ -4,13 +4,13 @@ tbname=online
 
 drop_mysql_memo_table $tbname	
 create_mysql_memo_table "create table $tbname ( \
-		uid		 	int 		primary key, \
+		uid		 	int 		primary key auto_increment, \
 		type 		char(16) 	not null default '', \
-		state		int 		not null default 0, \
+		state		int 		not null default 1, \
 		username 	char(64) 	not null unique default '', \
 		ip 			char(24) 	not null default '', \
 		mac 		char(24) 	not null default '', \
-		elapse 		int 		not null default 0, \
+		uptime 		int			not null default 0, \
 		flow 		int 		not null default 0 \
 	) engine=memory"
 
