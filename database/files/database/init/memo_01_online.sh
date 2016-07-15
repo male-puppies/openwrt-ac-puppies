0114d79cb706abbb74b9ecddee10ae7894b57c63
helloshiki <aquariusye@163.com>
2016-07-16 02:30:38 +0800

    re-define table onlone

--- database/files/database/init/memo_01_online.sh
+++ database/files/database/init/memo_01_online.sh
@@ -5,14 +5,14 @@ tbname=online
 drop_mysql_memo_table $tbname	
 create_mysql_memo_table "create table $tbname ( \
 		ukey	 	char(36)	primary key not null default '', \
-		type 		char(16) 	not null default '', \
-		state		int 		not null default 1, \
-		username 	char(64) 	not null default '', \
+		type 		char(8) 	not null default '', \
+		username 	char(40) 	not null default '', \
 		rid 		int 		not null default 0, \
 		ip 			char(24) 	not null default '', \
 		mac 		char(24) 	not null default '', \
 		login 		int			not null default 0, \
-		flow 		int 		not null default 0, \
+		rx 			bigint 		not null default 0, \
+		tx 			bigint 		not null default 0, \
 		active		int 		not null default 0 \
 	) engine=memory"
 
