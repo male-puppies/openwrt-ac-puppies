af7c96d4e47cbbf700d8ab716dd9107fdc633c5d
helloshiki <aquariusye@163.com>
2016-07-19 15:55:31 +0800

    dependance;offline_time

--- database/files/database/init/disk_08_kv.sh
+++ database/files/database/init/disk_08_kv.sh
@@ -11,6 +11,7 @@ create_sqlite3_disk_table $tbname $keyname "create table if not exists $tbname (
 	)"
 execute_sqlite3_disk_sql "insert or ignore into kv(k, v) values ('redirect_ip','1.0.0.8')"
 execute_sqlite3_disk_sql "insert or ignore into kv(k, v) values ('no_flow_timeout','1800')"
+execute_sqlite3_disk_sql "insert or ignore into kv(k, v) values ('offline_time','1800')"
 	
 drop_mysql_disk_table $tbname	
 create_mysql_disk_table "create table $tbname ( \
