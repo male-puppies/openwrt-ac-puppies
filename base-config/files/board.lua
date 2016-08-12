e18da3f1ab1e630505cba7e09d2c19f717dfe8a9
Chen Minqiang <ptpt52@gmail.com>
2016-08-12 23:16:07 +0800

    base-config: board.lua code cleanup

--- base-config/files/board.lua
+++ base-config/files/board.lua
@@ -8,24 +8,11 @@ local function load_board()
 	local s = read(path)	assert(s)
 	local m = js.decode(s)	assert(m)
 	local ports, options, networks = m.ports, m.options, m.networks	assert(ports and options and networks)
-	local port_map = {}
 
-	for _, dev in ipairs(ports) do
-		if dev.type == "switch" then
-			for idx, port in ipairs(dev.outer_ports) do
-				table.insert(port_map, {ifname=dev.ifname .. "." .. idx, mac = port.mac})
-			end
-		elseif dev.type == "ether" then
-			table.insert(port_map, {ifname=dev.ifname, mac = dev.outer_ports[1].mac})
-		end
-	end
-
-	return {ports = port_map, options = options, networks = networks}
+	return {options = options, networks = networks}
 end
 
 local board = load_board()
---local s = js.encode(m)
---print(s)
 
 local network = {}
 
