0243fa119a37182d526fbb9e6928ec03f153e368
Ivan <itgb1989@gmail.com>
2016-08-21 01:05:56 +0800

    fix invalid sql of acset

--- database/files/database_ar71xx/init/disk_18_acset.sh
+++ database/files/database_ar71xx/init/disk_18_acset.sh
@@ -9,7 +9,7 @@ create_sqlite3_disk_table $tbname $keyname "create table if not exists $tbname( 
 		setname	char(24)	not null unique default '',\
 		setdesc	char(128)	not null default '',\
 		setclass	char(8)		not null default '',\
-		settype 	char(8) 		not null defautl '', \
+		settype 	char(8) 		not null default '', \
 		content		text		, \
 		action		char(8)		not null default '',\
 		enable		integer 		not null default 1 \
@@ -20,7 +20,7 @@ create_mysql_disk_table "create table $tbname(\
 		setname	char(24)	not null unique default '',\
 		setdesc	char(128)	not null default '',\
 		setclass	char(8)		not null default '',\
-		settype 	char(8) 		not null defautl '',\
+		settype 	char(8) 		not null default '',\
 		content		text		, \
 		action		char(8)		not null default '',\
 		enable		integer 		not null default 1 \
