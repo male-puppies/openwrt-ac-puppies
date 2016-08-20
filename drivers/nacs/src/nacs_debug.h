33870d89c58a9a0c1dfff91ffac29b4392efc667
Ivan <itgb1989@gmail.com>
2016-08-21 05:40:24 +0800

    fix reference a invalid  klog fd

--- drivers/nacs/src/nacs_debug.h
+++ drivers/nacs/src/nacs_debug.h
@@ -1,15 +1,15 @@
 #ifndef _NACS_DEBUG_H
 #define _NACS_DEBUG_H
 #include <ntrack_log.h>
-extern void *nfw_klog_fd;
+extern void *nacs_klog_fd;
 #ifdef __DEBUG
 #define NACS_DEBUG(fmt,...)   do {printk(KERN_DEBUG fmt, ##__VA_ARGS__); } while(0)
 #else
-#define NACS_DEBUG(fmt...) klog_debug(nfw_klog_fd, ##fmt)
+#define NACS_DEBUG(fmt...) klog_debug(nacs_klog_fd, ##fmt)
 #endif
 
 #define NAC_PRINT_DEBUG(fmt,...)   do {printk(KERN_DEBUG fmt, ##__VA_ARGS__); } while(0)
-#define NACS_INFO(fmt...) 	klog_info(nfw_klog_fd, ##fmt)
-#define NACS_WARN(fmt...) 	klog_warn(nfw_klog_fd, ##fmt)
-#define NACS_ERROR(fmt...) 	klog_error(nfw_klog_fd, ##fmt)
+#define NACS_INFO(fmt...) 	klog_info(nacs_klog_fd, ##fmt)
+#define NACS_WARN(fmt...) 	klog_warn(nacs_klog_fd, ##fmt)
+#define NACS_ERROR(fmt...) 	klog_error(nacs_klog_fd, ##fmt)
 #endif
