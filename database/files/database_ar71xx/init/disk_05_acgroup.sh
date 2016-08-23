7a1f22ebf4d21638357c7051dc76fda59d89f051
helloshiki <aquariusye@163.com>
2016-08-23 15:47:18 +0800

    default group for acgroup

--- database/files/database_ar71xx/init/disk_05_acgroup.sh
+++ database/files/database_ar71xx/init/disk_05_acgroup.sh
@@ -5,17 +5,17 @@ keyname=gid
 
 #drop_sqlite3_disk_table $tbname
 create_sqlite3_disk_table $tbname $keyname "create table if not exists $tbname ( \
-		$keyname	integer 	primary key autoincrement,
+		$keyname	integer 	primary key,
 		groupname 	char(64) 	not null unique default '', \
 		groupdesc	char(128) 	not null default '', \
-		pid 		integer 	not null default 0 \
+		pid 		integer 	not null default -1 \
 	)"
 
 drop_mysql_disk_table $tbname	
 create_mysql_disk_table "create table $tbname ( \
-		$keyname	integer 	primary key autoincrement,
+		$keyname	integer 	primary key,
 		groupname 	char(64) 	not null unique default '', \
 		groupdesc	char(128) 	not null default '', \
-		pid 		integer 	not null default 0 \
+		pid 		integer 	not null default -1 \
 	)"
 	
--- database/files/database_ar71xx/init/disk_92_default.lua
+++ database/files/database_ar71xx/init/disk_92_default.lua
@@ -2,11 +2,12 @@
 
 package.path = "../?.lua;" .. package.path
 
-local lfs = require("lfs")
-local dc = require("dbcommon")
-local common = require("common")
-local js = require("cjson.safe")
-local config = require("config")
+local fp 	= require("fp")
+local lfs 	= require("lfs")
+local dc 	= require("dbcommon")
+local common 	= require("common")
+local js 		= require("cjson.safe")
+local config 	= require("config")
 
 local read, arr2map = common.read, common.arr2map
 local shpath = "../db.sh"
@@ -36,20 +37,20 @@ cmd_map.kv = {
 
 		local new_map = {
 			{k = "auth_bypass_dst", v = "[]"},
-			{k = "auth_offline_time", v = "1800"},
+			{k = "auth_offline_time", v = "8640000"},
 			{k = "auth_redirect_ip", v = "1.0.0.8"},
 			{k = "auth_no_flow_timeout", v = "1800"},
 		}
 
 		local miss, find = {}, false
-		for _, r in ipairs(new_map) do 
-			if not exist_map[r.k] then 
+		for _, r in ipairs(new_map) do
+			if not exist_map[r.k] then
 				miss[r.k], find = r, true
-			end 
+			end
 		end
 
-		if not find then 
-			return false 
+		if not find then
+			return false
 		end
 
 		local arr = {}
@@ -58,7 +59,7 @@ cmd_map.kv = {
 		end
 
 		local sql = string.format("insert into kv (k, v) values %s", table.concat(arr, ","))
-		local r, e = conn:execute(sql) 
+		local r, e = conn:execute(sql)
 		local _ = r or fatal("%s %s", sql , e)
 		return true
 	end
@@ -70,7 +71,7 @@ cmd_map.iface = {
 	func = function(conn)
 		local sql = "select count(*) as count from iface"
 		local rs, e = conn:select(sql) 				assert(rs, e)
-		if rs[1].count ~= 0 then 
+		if rs[1].count ~= 0 then
 			return
 		end
 
@@ -96,15 +97,15 @@ cmd_map.iface = {
 			dhcp_dns = "",
 		}
 		local set_default = function(r)
-			for field, v in pairs(default) do 
-				if not r[field] then 
+			for field, v in pairs(default) do
+				if not r[field] then
 					r[field] = v
 		end
 			end
 		end
 
 		local ifarr = require("parse_network").parse()
-		for _, r in ipairs(ifarr) do 
+		for _, r in ipairs(ifarr) do
 			set_default(r)
 			end
 		local ifmap = arr2map(ifarr, "ifname")
@@ -116,13 +117,13 @@ cmd_map.iface = {
 			r.zid, r.pid = n.zid, -1
 		end
 
-		-- set pid 
+		-- set pid
 		for _, r in pairs(ifmap) do
 			local parent = r.parent
-			if parent then 
+			if parent then
 				local n = ifmap[parent]
 				r.pid = n.zid
-			end 
+			end
 		end
 
 		local fields = {
@@ -153,9 +154,9 @@ cmd_map.iface = {
 		for i, r in ipairs(ifarr) do
 			local arr = {}
 			r.fid = i - 1
-			for _, field in ipairs(fields) do 
+			for _, field in ipairs(fields) do
 				table.insert(arr, string.format("'%s'", r[field]))
-		end 
+		end
 			local s = string.format("(%s)", table.concat(arr, ","))
 			table.insert(narr, s)
 		end
@@ -172,9 +173,9 @@ cmd_map.zone = {
 	func = function(conn)
 		local sql = "select count(*) as count from zone"
 		local rs, e = conn:select(sql) 				assert(rs, e)
-		if rs[1].count ~= 0 then 
-			return 
-		end 
+		if rs[1].count ~= 0 then
+			return false
+		end
 
 		local arr = {
 			{zid = 0, 	zonename = "LAN", zonedesc = "LAN", zonetype = 3},
@@ -183,11 +184,11 @@ cmd_map.zone = {
 		}
 
 		local narr = {}
-		for _, r in ipairs(arr) do 
+		for _, r in ipairs(arr) do
 			table.insert(narr, string.format("('%s','%s','%s','%s')", r.zid, r.zonename, r.zonedesc, r.zonetype))
 		end
 		local sql = string.format("insert into zone (zid,zonename,zonedesc,zonetype) values %s", table.concat(narr, ","))
-		local r, e = conn:execute(sql) 	
+		local r, e = conn:execute(sql)
 		local _ = r or fatal("%s %s", sql , e)
 		return true
 	end
@@ -198,9 +199,9 @@ cmd_map.ipgroup = {
 	func = function(conn)
 		local sql = "select count(*) as count from ipgroup"
 		local rs, e = conn:select(sql) 				assert(rs, e)
-		if rs[1].count ~= 0 then 
-			return 
-		end 
+		if rs[1].count ~= 0 then
+			return false
+		end
 
 		local arr = {
 			{ipgid = 63, ipgrpname = "ALL", ipgrpdesc = "ALL", ranges = js.encode({"0.0.0.0-255.255.255.255"})},
@@ -210,9 +211,37 @@ cmd_map.ipgroup = {
 		for _, r in ipairs(arr) do
 			table.insert(narr, string.format("('%s','%s','%s','%s')", r.ipgid, r.ipgrpname, r.ipgrpdesc, r.ranges))
 		end
+
 		local sql = string.format("insert into ipgroup (ipgid,ipgrpname,ipgrpdesc,ranges) values %s", table.concat(narr, ","))
-		local r, e = conn:execute(sql) 	
+		local r, e = conn:execute(sql)
 		local _ = r or fatal("%s %s", sql , e)
+
+		return true
+	end
+}
+
+cmd_map.acgroup = {
+	priority = 5,
+	func = function(conn)
+		local sql = "select count(*) as count from acgroup"
+		local rs, e = conn:select(sql) 				assert(rs, e)
+		if rs[1].count ~= 0 then
+			return false
+		end
+
+		local arr = {
+			{gid = 63, groupname = "default", groupdesc = "default", pid = -1},
+		}
+
+		local narr = fp.reduce(arr, function(t, r)
+			local s = string.format("('%s','%s','%s','%s')", r.gid, r.groupname, r.groupdesc, r.pid)
+			return rawset(t, #t + 1, s)
+		end, {})
+
+		local sql = string.format("insert into acgroup (gid,groupname,groupdesc,pid) values %s", table.concat(narr, ","))
+		local r, e = conn:execute(sql)
+		local _ = r or fatal("%s %s", sql , e)
+
 		return true
 	end
 }
@@ -222,9 +251,9 @@ cmd_map.timegroup = {
 	func = function(conn)
 		local sql = "select count(*) as count from timegroup where tmgrpname='ALL'"
 		local rs, e = conn:select(sql) 				assert(rs, e)
-		if rs[1].count ~= 0 then 
-			return 
-		end 
+		if rs[1].count ~= 0 then
+			return false
+		end
 
 		local days = {mon = 1, tues = 1, wed = 1, thur = 1, fri = 1, sat = 1, sun = 1}
 		local tmlist = {{hour_start = 0, min_start = 0, hour_end = 23, min_end = 59}}
@@ -236,9 +265,11 @@ cmd_map.timegroup = {
 		for _, r in ipairs(arr) do
 			table.insert(narr, string.format("('%s','%s','%s','%s','%s')", r.tmgid, r.tmgrpname, r.tmgrpdesc, r.days, r.tmlist))
 		end
+
 		local sql = string.format("insert into timegroup (tmgid,tmgrpname,tmgrpdesc,days,tmlist) values %s", table.concat(narr, ","))
-		local r, e = conn:execute(sql) 	
+		local r, e = conn:execute(sql)
 		local _ = r or fatal("%s %s", sql , e)
+
 		return true
 	end
 }
@@ -248,9 +279,9 @@ cmd_map.acset = {
 	func = function(conn)
 		local sql = "select count(*) as count from acset"
 		local rs, e = conn:select(sql) 				assert(rs, e)
-		if rs[1].count ~= 0 then 
-			return 
-		end 
+		if rs[1].count ~= 0 then
+			return false
+		end
 
 		local arr = {
 			{setid = 0, setname = "access_white_mac", setdesc = "access white mac", setclass = "control", settype = "mac", content = "[]", action = "bypass", enable = 1},
@@ -260,34 +291,38 @@ cmd_map.acset = {
 			{setid = 4, setname = "audit_white_mac", setdesc = "audit white mac", setclass = "audit", settype = "mac", content = "[]", action = "bypass", enable = 0},
 			{setid = 5, setname = "audit_white_ip", setdesc = "audit white ip", setclass = "audit", settype = "ip", content = "[]", action = "bypass", enable = 0},
 		}
-		
+
 		local narr = {}
 		for _, r in ipairs(arr) do
 			table.insert(narr, string.format("('%s','%s','%s','%s','%s','%s','%s','%s')", r.setid, r.setname, r.setdesc, r.setclass, r.settype, r.content, r.action, r.enable))
 		end
+
 		local sql = string.format("insert into acset (setid,setname,setdesc,setclass,settype,content,action,enable) values %s", table.concat(narr, ","))
-		local r, e = conn:execute(sql) 	
+		local r, e = conn:execute(sql)
 		local _ = r or fatal("%s %s", sql , e)
+
 		return true
 	end
 }
 
 local function main()
 	local cfg, e = config.ins() 					assert(cfg, e)
-	local conn = dc.new(cfg:get_workdb()) 
+	local conn = dc.new(cfg:get_workdb())
 
 	local arr = {}
-	for _, r in pairs(cmd_map) do 
+	for _, r in pairs(cmd_map) do
 		table.insert(arr, r)
-	end 
+	end
+
 	table.sort(arr, function(a, b) return a.priority < b.priority end)
+
 	local change = false
 	for _, r in pairs(arr) do
 		local r, e = r.func(conn)
 		change = change and change or r
 	end
 
-	if change then 
+	if change then
 		backup_disk(cfg)
 	end
 
