9278c5abe690225de5010192fdbf4048a88b6180
Chen Minqiang <ptpt52@gmail.com>
2016-08-24 15:06:57 +0800

    add md5 prefix for config version

--- performer/files/performer/authrule.lua
+++ performer/files/performer/authrule.lua
@@ -78,11 +78,11 @@ local function authrule_reload()
 
 		cmd = table.concat(cmd_arr, "\n")
 		new_md5 = md5.sumhexa(cmd)
-		old_md5 = common.read(string.format("uci get %s.@version[0].md5 | head -c32", name), io.popen)
+		old_md5 = common.read(string.format("uci get %s.@version[0].authrule_md5 | head -c32", name), io.popen)
 		print(new_md5, old_md5)
 		if new_md5 ~= old_md5 then
 			table.insert(cmd_arr, string.format("uci get %s.@version[0] || uci add %s version", name, name))
-			table.insert(cmd_arr, string.format("uci set %s.@version[0].md5='%s'", name, new_md5))
+			table.insert(cmd_arr, string.format("uci set %s.@version[0].authrule_md5='%s'", name, new_md5))
 			for _, line in ipairs(arr_cmd[name]) do
 				table.insert(cmd_arr, line)
 			end
--- performer/files/performer/network.lua
+++ performer/files/performer/network.lua
@@ -272,11 +272,11 @@ local function network_reload()
 
 		cmd = table.concat(arr[name], "\n")
 		new_md5 = md5.sumhexa(cmd)
-		old_md5 = common.read(string.format("uci get %s.@version[0].md5 | head -c32", name), io.popen)
+		old_md5 = common.read(string.format("uci get %s.@version[0].network_md5 | head -c32", name), io.popen)
 		--print(new_md5, old_md5)
 		if new_md5 ~= old_md5 then
 			table.insert(arr[name], string.format("uci get %s.@version[0] || uci add %s version", name, name))
-			table.insert(arr[name], string.format("uci set %s.@version[0].md5='%s'", name, new_md5))
+			table.insert(arr[name], string.format("uci set %s.@version[0].network_md5='%s'", name, new_md5))
 			for _, line in ipairs(arr_cmd[name]) do
 				table.insert(arr[name], line)
 			end
