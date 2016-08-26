381b053a2c71409f0b560e9127d136ed347c549e
Chen Minqiang <ptpt52@gmail.com>
2016-08-26 15:56:47 +0800

    fix bug

--- performer/files/performer/main.lua
+++ performer/files/performer/main.lua
@@ -16,7 +16,7 @@ local modules = {
 	dbevent = 	require("dbevent"),
 	acconfig = 	require("acconfig"),
 	authrule = 	require("authrule"),
-	authrule = 	require("firewall"),
+	firewall = 	require("firewall"),
 }
 
 local encode, decode = js.encode, js.decode
