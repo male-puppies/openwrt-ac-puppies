19d483cfcd9a6ca3ad9ff98cc44f2476ac659d33
Chen Minqiang <ptpt52@gmail.com>
2016-08-28 23:47:29 +0800

    check and fix white space from diff-index

--- 002-pre-commit-check-and-tryfix-space.sh
+++ 002-pre-commit-check-and-tryfix-space.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #
 
-git diff-index --check --cached HEAD --
+#git diff-index --check --cached HEAD --
+git diff-index --check HEAD --
 
-git diff-index --check --cached HEAD -- | sed '/^[+-]/d' | sed -E 's/:[0-9]+:.*//' | sort | uniq | while read line; do
+git diff-index --check HEAD -- | sed '/^[+-]/d' | sed -E 's/:[0-9]+:.*//' | sort | uniq | while read line; do
 	sed -E 's/[[:space:]]*$//' -i "$line"
 	echo auto fixing "$line"
-	git add "$line"
 done
